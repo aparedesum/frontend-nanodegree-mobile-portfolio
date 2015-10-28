45158d5f0df981ca79a18e8844b71aa9fe11ed7a
Alfonso Paredes <a.paredes.um@gmail.com>
2015-10-27 23:26:05 -0500

    adding original html and small images

--- views/js/main.js
+++ views/js/main.js
@@ -404,7 +404,7 @@ var pizzaElementGenerator = function(i) {
     pizzaContainer.id = "pizza" + i; // gives each pizza element a unique id
     pizzaImageContainer.classList.add("col-md-6");
 
-    pizzaImage.src = "images/pizza-min.png";
+    pizzaImage.src = "images/pizza.png";
     pizzaImage.classList.add("img-responsive");
     pizzaImageContainer.appendChild(pizzaImage);
     pizzaContainer.appendChild(pizzaImageContainer);
@@ -577,7 +577,7 @@ document.addEventListener('DOMContentLoaded', function() {
     for (var i = 0; i < totalOfPizzas; i++) {
         var elem = document.createElement('img');
         elem.className = 'mover';
-        elem.src = "images/pizza-min.png";
+        elem.src = "images/pizza.png";
         elem.basicLeft = (i % cols) * s;
         elem.style.left = elem.basicLeft + 'px';
         elem.style.top = (Math.floor(i / cols) * s) + 'px';
